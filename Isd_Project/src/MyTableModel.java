5dfc13cc68a0f367f5624be4f83e456b6da45c76
SatanChrist <prs.garg@gmail.com>
2018-12-12 00:27:34 +0530

    remove folder

--- Isd_Project/src/MyTableModel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.table.AbstractTableModel;
-
-public abstract class MyTableModel extends AbstractTableModel {
-
-    public boolean isCellEditable(int row, int column){  
-        return false;  
-    }
-
-}
