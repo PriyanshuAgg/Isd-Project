bfd8ebb699d3d6314a30311a152d5917b85e41c9
Paras Garg <31133978+SatanChrist@users.noreply.github.com>
2018-12-13 08:20:57 +0530

    Update TenderNoticee.java

--- Test/src/TenderNoticee.java
+++ Test/src/TenderNoticee.java
@@ -2,6 +2,11 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,27 +21,8 @@ public class TenderNoticee extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TenderNoticee frame = new TenderNoticee();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public TenderNoticee() {
-		this.setTitle("View App Purchases ");
+	public TenderNoticee(Application a) {
+		this.setTitle("Tender Notice");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 450);
 		contentPane = new JPanel();
@@ -52,7 +38,7 @@ public class TenderNoticee extends JFrame {
 		lblNewLabel.setBounds(66, 93, 119, 15);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblPurtitle = new JLabel("PurTitle");
+		JLabel lblPurtitle = new JLabel(a.getTitle());
 		lblPurtitle.setBounds(320, 93, 253, 15);
 		contentPane.add(lblPurtitle);
 		
@@ -60,9 +46,13 @@ public class TenderNoticee extends JFrame {
 		lblNewLabel_3.setBounds(66, 120, 119, 15);
 		contentPane.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
+		JLabel lblNewLabel_4 = new JLabel(a.getDesc());
 		lblNewLabel_4.setBounds(320, 120, 253, 15);
 		contentPane.add(lblNewLabel_4);
+		
+		MysqlCon sql = new MysqlCon();
+		Connection con = sql.Con();
+		ArrayList<Product> P = new ArrayList<Product>();
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(43, 212, 530, 140);
@@ -71,9 +61,21 @@ public class TenderNoticee extends JFrame {
 		JTable table = new JTable(model); 
 		model.addColumn("Name"); 
 		model.addColumn("Description");
-		model.addColumn("Reason for Purchase"); 
 		model.addColumn("Estimated Cost");
-		model.addRow(new Object [] {"<html><a href=\\\"\\\">abc</a></html>","gojfshf","fsdb","1000"});
+		
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from product where app_id="+a.getID()+";");
+			while(rs.next()) {
+				Product p = new Product(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getFloat(5),rs.getString(6),rs.getString(7));
+				P.add(p);
+				model.addRow(new Object [] {"<html><a href=\"\">"+p.getName()+"</a></html>", p.getDesc(), p.getCost()});
+			}
+		} catch (SQLException e1) {
+
+			e1.printStackTrace();
+		}
+		
 		scrollPane.setViewportView(table);
 		table.addMouseListener(new java.awt.event.MouseAdapter()
 		{																
@@ -83,9 +85,9 @@ public class TenderNoticee extends JFrame {
 				System.out.println("The Row selected is : "+row);
 				int col= table.columnAtPoint(e.getPoint());
 				if (col==0) {
-					Product p = new Product("a","a","a",123,"3eqq","fdfafa");
 					//int intn_id  = (int)model.getValueAt(row, col);
-					ViewProduct vap =  new ViewProduct(p);
+					
+					ViewProduct vap =  new ViewProduct(P.get(row));
 					vap.setVisible(true);
 				}
 			}
